44cdf412ab6c95ca7871f436f6ab8d200e1cdce8
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-08-15 21:33:11 +0530

    ui completed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+
+const num = [7, 8, 9,4, 5, 6, 1, 2, 3,0];
+const operator = ["/", "x", "-", "+", "="];
 
 function App() {
   return (
     <div className="calculator">
+      <div className="display" id="display">
+        10
+      </div>
 
-    <div className="display" id="display">10</div>
-    <button className="light-grey">1</button>
-    <button className="light-grey">2</button>
-    <button className="light-grey">3</button>
-    <button className="orange">4</button>
-    <button className="dark-grey">1</button>
-    <button className="dark-grey">2</button>
-    <button className="dark-grey">3</button>
-    <button className="orange">4</button>
-    <button className="dark-grey">1</button>
-    <button className="dark-grey">2</button>
-    <button className="dark-grey">3</button>
-    <button className="orange">4</button>
-    <button className="dark-grey">1</button>
-    <button className="dark-grey">2</button>
-    <button className="dark-grey">3</button>
-    <button className="orange">4</button>
-    <button className="dark-grey">1</button>
-    <button className="dark-grey">2</button>
-    <button className="dark-grey">3</button>
-    <button className="orange">4</button>
+      <div className="nums-container">
+      <button className="light-grey ac big-v">AC</button>
+        {num.map(n=>{
+          return <button className={`dark-grey ${n===0 && "big-v"}`}>{n}</button>
+        })}
+      <button className="light-grey">.</button>
+      </div>
+      <div className="ops-container">
+        {operator.map((o) => {
+
+          return (
+            <button key={o} className="orange">
+              {o}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
