b1f9090e81af658dc7c751f03cb129f7062128c0
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-08-15 20:48:10 +0530

    styling done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import "./App.css"
 
 function App() {
   return (
-    <div>Hello </div>
+    <div className="calculator">
+
+    <div className="display" id="display">10</div>
+    <button className="light-grey">1</button>
+    <button className="light-grey">2</button>
+    <button className="light-grey">3</button>
+    <button className="orange">4</button>
+    <button className="dark-grey">1</button>
+    <button className="dark-grey">2</button>
+    <button className="dark-grey">3</button>
+    <button className="orange">4</button>
+    <button className="dark-grey">1</button>
+    <button className="dark-grey">2</button>
+    <button className="dark-grey">3</button>
+    <button className="orange">4</button>
+    <button className="dark-grey">1</button>
+    <button className="dark-grey">2</button>
+    <button className="dark-grey">3</button>
+    <button className="orange">4</button>
+    <button className="dark-grey">1</button>
+    <button className="dark-grey">2</button>
+    <button className="dark-grey">3</button>
+    <button className="orange">4</button>
+    </div>
   );
 }
 
