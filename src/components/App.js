ffbab54a58c07fc49a4f75e0d24a503adaa28e0c
kritebh <kritebhlaganbibhakar@gmail.com>
2021-08-29 14:36:58 +0530

    reset button added

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React,{useState} from "react";
 import "./App.css";
 import "./Button"
 import Button from "./Button";
 
-const buttons = ["AC", "±", "%", "÷", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", "="];
+const buttons = ["AC", "±", "%", "÷", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
 
 function App() {
+
+  const [value,setValue] = useState("0")
+
+  const handleButtonClick =content=>()=>{
+    const num = parseFloat(value)
+
+    if(content==="AC"){
+      setValue("0");
+      return
+    }
+
+    if(content==='±'){
+      if(num<0){
+        setValue((num*-1).toString())
+        return;
+      }
+    }
+
+
+      setValue((num+content).toString())
+  }
+
+
+
+
+
   return (
     <div className="App">
       <div className="top">11:35</div>
-      <div className="display">0</div>
+      <div className="display">{value}</div>
       <div className="buttons">
         {
           buttons.map(b => {
             if (b === "AC" || b === "±" || b === "%") {
-              return <Button content={b} type="function"></Button>
+              return <Button content={b} type="function" onButtonClick={handleButtonClick}></Button>
             }
             else if (b === "÷" || b === "x" || b === "-" || b === "+" || b === "=") {
-              return <Button content={b} type="operator"></Button>
+              return <Button content={b} type="operator" onButtonClick={handleButtonClick}></Button>
             }
             return (
-              <Button content={b}></Button>
+              <Button content={b} onButtonClick={handleButtonClick}></Button>
             )
           })
         }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./Button.css";
 
-const Button = ({content,type}) =>{
+const Button = ({content,type,onButtonClick}) =>{
     return(
-        <div className={`Button ${content===0 ? 'zero':''} ${type||""}`} >
+        <div className={`Button ${content===0 ? 'zero':''} ${type||""}`} onClick={onButtonClick(content)} >
         {content}
         </div>
     )
